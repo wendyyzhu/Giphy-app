f19d56c89000d5b8c1f38a7c36c719e3f57b1ca3
Chatkamon <chatkamon_bee@hotmail.com>
2023-08-01 09:32:54 +1000

    trending section

--- giphy-app/src/Trending.jsx
+++ giphy-app/src/Trending.jsx
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react"
+import './Trending.css'
+
 export default function Trending(){
+
+  const [trendyGif, setTrendyGif] = useState([])
+  const [limit, setLimit] = useState(3)
+  
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_NOT_SECRET_CODE;
+    const apiUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${limit}`;
+
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => setTrendyGif(data.data))
+    .catch((error) => console.error('Error fetching trendy GIFs:', error));
+  }, [limit]);
+
+  function handleLimit(e) {
+    setLimit(e.target.value)
+  }
+
   return (
-    <div>
+    <div className="trending-container">
       <h2>Trendy Gif</h2>
+      <div className="limit-controls">
+        <label>Choose the number of Trendy GIFs:</label>
+        <input
+        type="number"
+        value={limit}
+        onChange={handleLimit}
+        min="1"
+        max="20"
+        />
+        <h3>Top {limit} Today</h3>
+      </div>
+
+      <div className="gif-container">
+      {trendyGif.map((gif, index) => (
+          <div key={gif.id} className={`gif-box ${index < 3 ? 'top-trending' : ''}`}>
+          {index < 3 && (
+            <div className="top-trending-tag">
+              👑 Top {index + 1}
+            </div>
+          )}
+          <img src={gif.images.fixed_height.url} alt="Trendy Gif" className="gif-image" />
+          <p>{gif.title}</p>
+        </div>
+        ))}
+      </div>
     </div>
   )
 }
